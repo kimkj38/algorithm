f96ac61e52d55b39785b4a1a92f6aff58561f7d4
kimkj38 <76815825+kimkj38@users.noreply.github.com>
2021-07-06 15:22:25 +0900

    Update 상하좌우.py

--- 구현/상하좌우.py
+++ 구현/상하좌우.py
@@ -19,24 +19,3 @@ for command in commands:
    
 print(x, y)
   
-# 교재 답안
-n = int(input())
-x,y = 1, 1
-plans = input().split()
-
-dx = [0, 0, -1, 1]
-dy = [-1, 1, 0, 0]
-move_types = ['L','R','U','D']
-
-for plan in plans:
-    for i range(len(move_types)):
-        if plan == move_types[i]:
-            nx = x + dx[i]
-            ny = y + dy[i]
-    if nx < 1 or ny < 1 or nx > n or ny > n:
-        continue
-    
-    x, y = nx, ny
-    
-print(x, y)
-     
