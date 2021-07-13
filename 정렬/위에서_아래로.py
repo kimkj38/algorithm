d7017f236f772be133e86a635213be8219e12e86
kimkj38 <76815825+kimkj38@users.noreply.github.com>
2021-07-13 14:16:11 +0900

    Create 위에서_아래로.py

---
+++ 정렬/위에서_아래로.py
@@ -0,0 +1,10 @@
+N = int(input())
+num_list = []
+
+for _ in range(N):
+    num_list.append(int(input()))
+    
+num_list = sorted(num_list, reverse=True)
+
+for i in num_list:
+    print(i, end=' ')
