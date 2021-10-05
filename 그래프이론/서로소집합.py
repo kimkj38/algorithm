2e76f83c6fb37515a1590d93d7dfa18ffc13a1fa
kimkj38 <76815825+kimkj38@users.noreply.github.com>
2021-10-05 23:27:01 +0900

    Update 서로소집합.py

--- 그래프이론/서로소집합.py
+++ 그래프이론/서로소집합.py
@@ -5,6 +5,12 @@ def find_parent(parent, x):
         return find_parent(parent, parent[x])
     return x
 
+#경로 압축
+def find_parent(parent, x):
+    if parent[x] != x:
+        parent[x] = find_parent(parent, parent[x])
+    return parent[x]
+
 #두 원소가 속한 집합 합치기
 def union_parent(parent, a, b):
     a = find_parent(parent, a)
